1d76db3a5dd7562c531b0213791353b494a945af
Yann Jorelle <yann.jorelle@nokia.com>
2020-07-08 10:45:52 +0300

    Use react instead of jekyll for github-pages
    Signed-off-by: Yann Jorelle <yann.jorelle@nokia.com>

---
+++ precache-manifest.b6be820c0945c64319a3a49ace7cd0a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c7096060780f2cf89bd00fe44ad5aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1015ab3440c91f0d9cd4",
+    "url": "/static/js/2.9fdbf44d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9fdbf44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6de4264bfb62f9b957d",
+    "url": "/static/js/main.c65667b5.chunk.js"
+  },
+  {
+    "revision": "ce7cf9055f320d4e507c",
+    "url": "/static/js/runtime-main.aef3c844.js"
+  }
+]);
---
+++ precache-manifest.e08e9875f602001a422796ac2962ada8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e52e0d6606807f872afb1ac34e86e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1015ab3440c91f0d9cd4",
+    "url": "/static/js/2.9fdbf44d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9fdbf44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae11229be9a1dfd8916",
+    "url": "/static/js/main.bee8c2f4.chunk.js"
+  },
+  {
+    "revision": "ce7cf9055f320d4e507c",
+    "url": "/static/js/runtime-main.aef3c844.js"
+  }
+]);
