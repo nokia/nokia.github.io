24360f6c94d5d531b171142bd13ea895aba9d0e0
Yann Jorelle <yann.jorelle@nokia.com>
2020-07-14 10:39:50 +0300

    Change to hashRouter from browserRouter to fix gh-pages routing problem
    Signed-off-by: Yann Jorelle <yann.jorelle@nokia.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
 import Home from "./Home";
