58acd2fc073028e714c76cc6f1b237f2abf3c2d8
Yann Jorelle <yann.jorelle@nokia.com>
2020-07-14 15:59:04 +0300

    Add links to cards and improve card style
    Signed-off-by: Yann Jorelle <yann.jorelle@nokia.com>

--- src/components/FeaturedRepo.js
+++ src/components/FeaturedRepo.js
@@ -3,32 +3,53 @@ import { Card } from "react-bootstrap";
 import { trim } from "../utils/utils";
 
 const FeaturedRepo = ({ name, repo_url, homepage, description, icon }) => {
-  const trimLength = icon ? 75 : 150; // Length to trim description.
-
   const cardStyle = {
     minWidth: "18rem",
-    minHeight: "21rem",
+    minHeight: "18rem",
     marginBottom: "3rem",
   };
 
-  const cardImageStyle = {
-    maxWidth: "9rem",
+  const cardImageDivStyle = {
+    width: "15rem",
+    height: "9rem",
     display: "block",
-    margin: "auto",
-    marginTop: "1rem",
+    margin: "0 auto",
+    marginBlock: "1rem",
+  };
+
+  const cardImageStyle = {
+    maxWidth: "55%",
+    display: "block",
+    margin: "0 auto",
   };
 
   return (
     <>
-      <a href={homepage ? homepage : repo_url} className="repoCard">
+      <div className="repoCard">
         <Card style={cardStyle}>
-          <Card.Img variant="top" src={icon} style={cardImageStyle} />
-          <Card.Body style={icon ? {} : { marginTop: "8rem" }}>
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>{trim(description, trimLength)}</Card.Text>
+          <div className="cardImage" style={cardImageDivStyle}>
+            <Card.Img variant="top" src={icon} style={cardImageStyle} />
+          </div>
+          <Card.Body>
+            <div className="cardText">
+              <a href={homepage ? homepage : repo_url}>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{trim(description, 113)}</Card.Text>
+              </a>
+            </div>
+            <div className="cardLinks">
+              <Card.Link href={repo_url}>
+                <i className="fa fa-github"></i>
+              </Card.Link>
+              {homepage && (
+                <Card.Link href={homepage}>
+                  <i className="fa fa-home"></i>
+                </Card.Link>
+              )}
+            </div>
           </Card.Body>
         </Card>
-      </a>
+      </div>
     </>
   );
 };
